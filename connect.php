e52512bdaf22b34d4fa58280de9f40add8a48f9b
Undem0ns <andamannnira@kkumail.com>
2021-08-23 11:03:09 +0700

    add css

--- connect.php
+++ connect.php
@@ -1,4 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap\css\bootstrap-grid.min.css">
+
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
 <?php
+
     $hostName = "localhost";
     $userName = "root";
     $password = "";
@@ -8,8 +23,8 @@
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
     }else{
-        echo "Connected successfully";
-        echo "<br>";
+        // echo "Connected successfully";
+        // echo "<br>";
         // console.log($conn);
     }
     
