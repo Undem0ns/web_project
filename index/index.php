f4f63485c57094fa61e1a74aa06add2e20e00188
bmnat <natthidawsm@kkumail.com>
2021-08-23 14:09:04 +0700

    create index as navbar

---
+++ index/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <?php include "../connect.php" ?>
+
+</head>
+<body>
+  <nav class="navbar navbar-light bg-secondary fixed-top">
+  <div class="container-fluid">
+    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample" aria-controls="offcanvasNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <ul class="nav justify-content-end">
+        <!-- <li class="nav-item">
+          <a class=nav-link active" aria-current="page" href="#"><?php echo $row['username']; ?></a>
+        </li> -->
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">ออกจากระบบ</a>
+        </li>
+    </ul>
+    <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel" >
+      <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasExampleLabel">PTP-MS</h5>
+        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+      </div>
+      <div class="offcanvas-body">
+        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">เพิ่มโครงการ</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">รายชื่อโครงการ</a>
+          </li>
+      </div>
+      
+  </nav>
+
+</body>
+</html>
