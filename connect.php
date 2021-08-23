7989b9b384789743cc68eac9e95378452a4d52a9
yannapat112 <yannapat@kkumail.com>
2021-08-23 16:24:44 +0700

    login

--- connect.php
+++ connect.php
@@ -8,6 +8,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
     <title>Document</title>
 </head>
@@ -16,20 +17,13 @@
 </body>
 </html>
 <?php
-
-    $hostName = "localhost";
-    $userName = "root";
-    $password = "";
-    // Create connection
-    $conn = mysqli_connect($hostName, $userName, $password);
-    // Check connection
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    }else{
-        // echo "Connected successfully";
-        // echo "<br>";
-        // console.log($conn);
-    }
-    
-    // mysqli_close($conn);
+try {
+$pdo = new PDO("mysql:host=localhost;dbname=pcp_ms;charset=utf8", "root", "");
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+echo "เกิดข้อผิดพลาด : " . $e->getMessage();
+}
 ?>
+<html>
+<body></body>
+</html>
--- login/login_check.php
+++ login/login_check.php
@@ -1,20 +1,34 @@
-<?php include '../connect.php' ?>
-<?php session_start(); ?>
+
+
 <?php
-$stmt = $pdo->prepare("SELECT * FROM ... WHERE ... = ? AND ... = ?");
-$stmt->bindParam(1, $_POST["email"]);
-$stmt->bindParam(2, $_POST["em_pass"]);
+    include '../connect.php';
+    session_start();
+$stmt = $pdo->prepare("SELECT * FROM user WHERE user_email = ? AND user_password = ?");
+$stmt->bindParam(1, $_POST["user_email"]);
+$stmt->bindParam(2, $_POST["user_password"]);
 $stmt->execute();
 $row = $stmt->fetch();
 
 if(!empty($row)){
     
-    $_SESSION["email"] = $row["em_pass"];
-    header("Location: list1.php");
+    $_SESSION["user_email"] = $row["user_password"];  
+    header("Location:../index/index.php");
 }else{
     echo "เข้าสู่ระบบล้มเหลว" . "<br>";
     echo "กลับไปยังหน้าเข้าสู่ระบบใน 3 วินาที" . "<br>";
-    echo "<a href='login/index.php'>คลิกเพื่อเข้าสู่ระบบอีกครั้ง</a>";
-    echo "<meta http-equiv='refresh' content='3,URL=login/index.php' />";
+    echo "<a href='index.php'>คลิกเพื่อเข้าสู่ระบบอีกครั้ง</a>";
+    echo "<meta http-equiv='refresh' content='3,URL=index.php' />";
 }
+
 ?>
+
+
+<?php 
+//  if(isset($_POST['submit'])){
+//     session_start();
+//     $_SESSION['e_mail'] = htmlentities($_POST['e_mail']);
+//     header('location: test.php  ');
+//  }
+
+?>
+
