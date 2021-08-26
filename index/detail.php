58d17f3afb92712288a62663ba78d16a31cfc547
bmnat <natthidawsm@kkumail.com>
2021-08-26 16:15:23 +0700

    add icon profile

--- index/detail.php
+++ index/detail.php
@@ -8,7 +8,6 @@
   <title>Document</title>
   <?php include "../connect.php" ?>
 </head>
-
 <body>
   <?php
   session_start();
@@ -16,8 +15,7 @@
     header("location:../login/index.php");
   }
 
-  $stmt = $pdo->prepare("SELECT * FROM project where project_id = ?");
-  $stmt->bindParam(1, $_GET["project_id"]);
+  $stmt = $pdo->prepare("SELECT * FROM project");
   $stmt->execute();
   ?>
 
@@ -43,33 +41,36 @@
           </li>
         </ul>
         <ul class="nav justify-content-end">
-          <!-- <li class="nav-item">
-              <a class=nav-link active" aria-current="page" href="#"><?php echo $row['user_name']; ?></a>
-        </li> -->
+        <li class="nav-item">
+              <a class=nav-link active aria-current="page" ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+  <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+</svg>  <?php echo $_COOKIE["user_email"]; ?></a>
+        </li>
           <li class="nav-item">
             <a class="nav-link active" aria-current="page" href="../login/index.php">ออกจากระบบ</a>
           </li>
         </ul>
   </header>
-  <?php $row = $stmt->fetch()  ?>
+
   <div id="main" class="main bd-main order-1">
     <div class="bd-intro ps-lg-4">
       <div class="d-md-flex flex-md-row-reverse align-items-center justify-content-between">
         <!-- <button type="button" class="btn btn-secondary">Secondary</button> -->
-        <a class="btn btn-danger" href="edit_project.php?project_id=<?= $row["project_id"] ?>" title="edit" rel="noopener" style="margin-right: 20px;"><svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
+        <a class="btn btn-danger" href="edit.php" title="edit" rel="noopener" style="margin-right: 20px;"><svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
             <path d="M8 0a1 1 0 0 1 1 1v6h6a1 1 0 1 1 0 2H9v6a1 1 0 1 1-2 0V9H1a1 1 0 0 1 0-2h6V1a1 1 0 0 1 1-1z" />
           </svg> แก้ไข </a>
         <h2 class="bd-title" id="content" style="font-weight: bold;">รายละเอียดโครงการ</h2>
       </div>
     </div>
     <br>
-    <div class="w-100 bd-highlight">
-      <div class="row mb-3">
-        <label for="project_code" class="col-sm-2 col-form-label" style="font-weight: bold;">ชื่อโครงการ/ชื่อกิจกรรม/ชื่อการดำเนินงาน:</label>
-        <div class="col-sm-10">
-          <?= $row["project_name"] ?>
-        </div>
+    <div class="w-100 bd-highlight">        
         <div class="row mb-3">
+          <label for="project_code" class="col-sm-2 col-form-label" style="font-weight: bold;">ชื่อโครงการ/ชื่อกิจกรรม/ชื่อการดำเนินงาน:</label>
+          <div class="col-sm-10">
+            <?= $row["project_name"] ?>
+          </div>
+          <div class="row mb-3">
           <div class="-flex p-2 bd-highlight" style="background-color: gray;">
             <label>ความเชื่อมโยงของโครงการกับแผนกลุ่มจังหวัด
             </label>
@@ -78,211 +79,210 @@
         <div class="row mb-3">
           <label for="project_code" class="col-sm-2 col-form-label" style="font-weight: bold;">ประเด็นการพัฒนา: </label>
           <div class="col-sm-10">
-            <?= $row["development_subject"] ?>
+            <?= $row[""] ?>
           </div>
         </div>
         <div class="row mb-3">
           <label for="project_code" class="col-sm-2 col-form-label" style="font-weight: bold;">แผนงาน</label>
           <div class="col-sm-10">
-            <?= $row["project_roadmap"] ?>
+            <?= $row[""] ?>
           </div>
         </div>
         <div class="row mb-3">
           <label for="project_code" class="col-sm-2 col-form-label" style="font-weight: bold;">โครงการหลัก (ตามแผน)</label>
           <div class="col-sm-10">
-            <?= $row["project_main"] ?>
+            <?= $row[""] ?>
           </div>
         </div>
-      </div>
-      <div class="row mb-3">
-        <label for="project_code" class="col-sm-2 col-form-label" style="font-weight: bold;">โครงการ/กิจกรรมย่อย (ตามแผน)</label>
-        <div class="col-sm-10">
-          <?= $row["project_sub"] ?>
+    </div>
+    <div class="row mb-3">
+          <label for="project_code" class="col-sm-2 col-form-label" style="font-weight: bold;">โครงการ/กิจกรรมย่อย (ตามแผน)</label>
+          <div class="col-sm-10">
+            <?= $row[""] ?>
+          </div>
         </div>
-      </div>
-      <div class="row mb-3">
-        <div class="-flex p-2 bd-highlight" style="background-color: gray;">
-          <label>ภาพรวมการดำเนินงาน</label>
+        <div class="row mb-3">
+          <div class="-flex p-2 bd-highlight" style="background-color: gray;">
+            <label>ภาพรวมการดำเนินงาน</label>
+          </div>
         </div>
-      </div>
-      <div class="row mb-3">
-        <label for="project_code" class="col-sm-2 col-form-label" style="font-weight: bold;">ลักษณะการดำเนินงาน</label>
-        <div class="col-sm-10">
-          <?= $row["operation_type"] ?>
+        <div class="row mb-3">
+          <label for="project_code" class="col-sm-2 col-form-label" style="font-weight: bold;">ลักษณะการดำเนินงาน</label>
+          <div class="col-sm-10">
+            <?= $row[""] ?>
+          </div>
         </div>
-      </div>
-      <div class="row mb-3">
-        <label for="project_code" class="col-sm-2 col-form-label" style="font-weight: bold;">รหัสโครงงาน (ถ้ามี)</label>
-        <div class="col-sm-10">
-          <?= $row["project_code"] ?>
+        <div class="row mb-3">
+          <label for="project_code" class="col-sm-2 col-form-label" style="font-weight: bold;">รหัสโครงงาน (ถ้ามี)</label>
+          <div class="col-sm-10">
+            <?= $row[""] ?>
+          </div>
         </div>
-      </div>
-      <div class="row mb-3">
-        <label for="project_code" class="col-sm-2 col-form-label" style="font-weight: bold;">การใช้งบประมาณ</label>
-        <div class="col-sm-10">
-          <?= $row["use_budget"] ?>
+        <div class="row mb-3">
+          <label for="project_code" class="col-sm-2 col-form-label" style="font-weight: bold;">การใช้งบประมาณ</label>
+          <div class="col-sm-10">
+            <?= $row[""] ?>
+          </div>
         </div>
-      </div>
-      <div class="row mb-3">
-        <label for="project_code" class="col-sm-2 col-form-label" style="font-weight: bold;">ปีงบประมาณที่ดำเนินโครงการ</label>
-        <div class="col-sm-10">
-          <?= $row["budget_year"] ?>
+        <div class="row mb-3">
+          <label for="project_code" class="col-sm-2 col-form-label" style="font-weight: bold;">ปีงบประมาณที่ดำเนินโครงการ</label>
+          <div class="col-sm-10">
+            <?= $row[""] ?>
+          </div>
         </div>
-      </div>
-      <div class="row mb-3">
-        <label for="project_code" class="col-sm-2 col-form-label" style="font-weight: bold;">หน่วยงานที่รับผิดชอบ</label>
-        <div class="col-sm-10">
-          <?= $row["institution"] ?>
+        <div class="row mb-3">
+          <label for="project_code" class="col-sm-2 col-form-label" style="font-weight: bold;">หน่วยงานที่รับผิดชอบ</label>
+          <div class="col-sm-10">
+            <?= $row[""] ?>
+          </div>
         </div>
-      </div>
-      <div class="row mb-3">
-        <label for="project_code" class="col-sm-2 col-form-label" style="font-weight: bold;">จังหวัดที่ดำเนินโครงการ</label>
-        <div class="col-sm-10">
-          <?= $row["province"] ?>
+        <div class="row mb-3">
+          <label for="project_code" class="col-sm-2 col-form-label" style="font-weight: bold;">จังหวัดที่ดำเนินโครงการ</label>
+          <div class="col-sm-10">
+            <?= $row[""] ?>
+          </div>
         </div>
-      </div>
-      <div class="row mb-3">
-        <label for="project_code" class="col-sm-2 col-form-label" style="font-weight: bold;">กลุ่มเป้าหมายในการดำเนินงาน</label>
-        <div class="col-sm-10">
-          <?= $row["target_user"] ?>
+        <div class="row mb-3">
+          <label for="project_code" class="col-sm-2 col-form-label" style="font-weight: bold;">กลุ่มเป้าหมายในการดำเนินงาน</label>
+          <div class="col-sm-10">
+            <?= $row[""] ?>
+          </div>
         </div>
-      </div>
-      <div class="row mb-3">
-        <div class="-flex p-2 bd-highlight" style="background-color: gray;">
-          <label>รายละเอียดการดำเนินงาน</label>
+        <div class="row mb-3">
+          <div class="-flex p-2 bd-highlight" style="background-color: gray;">
+            <label>รายละเอียดการดำเนินงาน</label>
+          </div>
         </div>
-      </div>
-      <div class="row mb-3">
-        <label for="project_code" class="col-sm-2 col-form-label" style="font-weight: bold;">วัตถุประสงค์และรายละเอียดของโครงการ</label>
-        <div class="col-sm-10">
-          <?= $row["project_objective"] ?>
+        <div class="row mb-3">
+          <label for="project_code" class="col-sm-2 col-form-label" style="font-weight: bold;">วัตถุประสงค์และรายละเอียดของโครงการ</label>
+          <div class="col-sm-10">
+            <?= $row[""] ?>
+          </div>
         </div>
-      </div>
-      <div class="row mb-3">
-        <label for="project_code" class="col-sm-2 col-form-label" style="font-weight: bold;">ผลผลิต/ผลลัพธ์ ตามตัวชี้วัด</label>
-        <div class="col-sm-10">
-          <?= $row["project_result"] ?>
+        <div class="row mb-3">
+          <label for="project_code" class="col-sm-2 col-form-label" style="font-weight: bold;">ผลผลิต/ผลลัพธ์ ตามตัวชี้วัด</label>
+          <div class="col-sm-10">
+            <?= $row[""] ?>
+          </div>
         </div>
-      </div>
-      <div>
-        <div class="-flex p-2 bd-highlight" style="background-color: gray;">
-          <label>แหล่งงบประมาณตามแผน</label>
-        </div>
+        <div>
+          <div class="-flex p-2 bd-highlight" style="background-color: gray;">
+            <label>แหล่งงบประมาณตามแผน</label>
+          </div>
         <div class="row mb-3">
           <label for="project_code" class="col-sm-2 col-form-label" style="font-weight: bold;">งบประมาณของจังหวัด/กลุ่มจังหวัด</label>
           <div class="col-sm-10">
-            จำนวน <?= $row["budget_province_plan"] ?> บาท
+            จำนวน <?= $row[""] ?> บาท
           </div>
         </div>
         <div class="row mb-3">
           <label for="project_code" class="col-sm-2 col-form-label" style="font-weight: bold;">งบประมาณของกระทรวง/กรม</label>
           <div class="col-sm-10">
-            จำนวน <?= $row["budget_department_plan"] ?> บาท
+            จำนวน <?= $row[""] ?> บาท
           </div>
         </div>
         <div class="row mb-3">
           <label for="project_code" class="col-sm-2 col-form-label" style="font-weight: bold;">งบประมาณขององค์กรปกครองส่วนท้องถิ่น</label>
           <div class="col-sm-10">
-            จำนวน <?= $row["budget_local_plan"] ?> บาท
+          จำนวน <?= $row[""] ?> บาท
           </div>
         </div>
         <div class="row mb-3">
           <label for="project_code" class="col-sm-2 col-form-label" style="font-weight: bold;">งบประมาณของภาคเอกชน/ชุมชน</label>
           <div class="col-sm-10">
-            จำนวน <?= $row["budget_private_plan"] ?> บาท
+          จำนวน <?= $row[""] ?> บาท
           </div>
         </div>
         <div class="-flex p-2 bd-highlight" style="background-color: gray;">
-          <label>งบประมาณที่ได้รับจัดสรร</label>
-        </div>
+            <label>งบประมาณที่ได้รับจัดสรร</label>
+          </div>
         <div class="row mb-3">
           <label for="project_code" class="col-sm-2 col-form-label" style="font-weight: bold;">งบประมาณของจังหวัด/กลุ่มจังหวัด</label>
           <div class="col-sm-10">
-            จำนวน <?= $row["budget_province_receive"] ?> บาท
+            จำนวน <?= $row[""] ?> บาท
           </div>
         </div>
         <div class="row mb-3">
           <label for="project_code" class="col-sm-2 col-form-label" style="font-weight: bold;">งบประมาณของกระทรวง/กรม</label>
           <div class="col-sm-10">
-            จำนวน <?= $row["budget_department_receive"] ?> บาท
+            จำนวน <?= $row[""] ?> บาท
           </div>
         </div>
         <div class="row mb-3">
           <label for="project_code" class="col-sm-2 col-form-label" style="font-weight: bold;">งบประมาณขององค์กรปกครองส่วนท้องถิ่น</label>
           <div class="col-sm-10">
-            จำนวน <?= $row["budget_local_receive"] ?> บาท
+          จำนวน <?= $row[""] ?> บาท
           </div>
         </div>
         <div class="row mb-3">
           <label for="project_code" class="col-sm-2 col-form-label" style="font-weight: bold;">งบประมาณของภาคเอกชน/ชุมชน</label>
           <div class="col-sm-10">
-            จำนวน <?= $row["budget_private_receive"] ?> บาท
+          จำนวน <?= $row[""] ?> บาท
           </div>
         </div>
         <div class="-flex p-2 bd-highlight" style="background-color: gray;">
-          <label>งบประมาณตามที่ใช้จ่ายจริง</label>
-        </div>
-        <div class="row mb-3">
+            <label>งบประมาณตามที่ใช้จ่ายจริง</label>
+          </div>
+          <div class="row mb-3">
           <label for="project_code" class="col-sm-2 col-form-label" style="font-weight: bold;">งบประมาณของจังหวัด/กลุ่มจังหวัด</label>
           <div class="col-sm-10">
-            จำนวน <?= $row["budget_province_use"] ?> บาท
+            จำนวน <?= $row[""] ?> บาท
           </div>
         </div>
         <div class="row mb-3">
           <label for="project_code" class="col-sm-2 col-form-label" style="font-weight: bold;">งบประมาณของกระทรวง/กรม</label>
           <div class="col-sm-10">
-            จำนวน <?= $row["budget_department_use"] ?> บาท
+            จำนวน <?= $row[""] ?> บาท
           </div>
         </div>
         <div class="row mb-3">
           <label for="project_code" class="col-sm-2 col-form-label" style="font-weight: bold;">งบประมาณขององค์กรปกครองส่วนท้องถิ่น</label>
           <div class="col-sm-10">
-            จำนวน <?= $row["budget_local_use"] ?> บาท
+          จำนวน <?= $row[""] ?> บาท
           </div>
         </div>
         <div class="row mb-3">
           <label for="project_code" class="col-sm-2 col-form-label" style="font-weight: bold;">งบประมาณของภาคเอกชน/ชุมชน</label>
           <div class="col-sm-10">
-            จำนวน <?= $row["budget_private_use"] ?> บาท
+          จำนวน <?= $row[""] ?> บาท
           </div>
         </div>
         <div class="-flex p-2 bd-highlight" style="background-color: gray;">
           <label>รายละเอียดอื่น ๆ เพิ่มเติม</label>
         </div><br>
-
+     
         <div class="row mb-3">
           <label for="project_code" class="col-sm-2 col-form-label" style="font-weight: bold;">ลิงค์หรือรายละเอียดอื่น ๆ <br>เพิ่มเติมที่น่าจะเป็นประโยชน์</label>
           <div class="col-sm-10">
-            <?= $row["project_additional"] ?>
+            <?= $row[""] ?>
           </div>
         </div>
         <div class="row mb-3">
           <label for="project_code" class="col-sm-2 col-form-label" style="font-weight: bold;">ไฟล์แนบ (ถ้ามี)</label>
           <div class="col-sm-10">
-            <!-- <?= $row[""] ?> -->
+            <?= $row[""] ?>
           </div>
         </div>
 
-      </div>
-    </div>
+    </div></div>
+    
 
 
 
 
 
 
+  <script>
+    function openNav() {
+      document.getElementById("mySidenav").style.width = "250px";
+      document.getElementById("main").style.marginLeft = "250px";
+    }
 
-    <script>
-      function openNav() {
-        document.getElementById("mySidenav").style.width = "250px";
-        document.getElementById("main").style.marginLeft = "250px";
-      }
-
-      function closeNav() {
-        document.getElementById("mySidenav").style.width = "0";
-        document.getElementById("main").style.marginLeft = "0";
-      }
-    </script>
+    function closeNav() {
+      document.getElementById("mySidenav").style.width = "0";
+      document.getElementById("main").style.marginLeft = "0";
+    }
+  </script>
 </body>
 
 </html>
