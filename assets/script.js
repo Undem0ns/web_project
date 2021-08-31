6c6c97a60290c811a9c8b60c480017006a2bc2ab
Undem0ns <andamannnira@kkumail.com>
2021-08-31 16:36:59 +0700

    add drop down

---
+++ assets/script.js
@@ -0,0 +1,65 @@
+$(function(){
+    var development_subjectObject = $('#development_subject');
+    var project_roadmapObject = $('#project_roadmap');
+    var project_mainObject = $('#project_main');
+    var project_subObject = $('#project_sub');
+
+    // on change province
+    development_subjectObject.on('change', function(){
+        var provinceId = $(this).val();
+
+        amphureObject.html('<option value="">เลือกอำเภอ</option>');
+        districtObject.html('<option value="">เลือกตำบล</option>');
+
+        $.get('get_amphure.php?province_id=' + provinceId, function(data){
+            var result = JSON.parse(data);
+            $.each(result, function(index, item){
+                amphureObject.append(
+                    $('<option></option>').val(item.id).html(item.name_th)
+                );
+            });
+        });
+    });
+
+
+
+
+
+
+    var provinceObject = $('#province');
+    var amphureObject = $('#amphure');
+    var districtObject = $('#district');
+
+    // on change province
+    provinceObject.on('change', function(){
+        var provinceId = $(this).val();
+
+        amphureObject.html('<option value="">เลือกอำเภอ</option>');
+        districtObject.html('<option value="">เลือกตำบล</option>');
+
+        $.get('get_amphure.php?province_id=' + provinceId, function(data){
+            var result = JSON.parse(data);
+            $.each(result, function(index, item){
+                amphureObject.append(
+                    $('<option></option>').val(item.id).html(item.name_th)
+                );
+            });
+        });
+    });
+
+    // on change amphure
+    amphureObject.on('change', function(){
+        var amphureId = $(this).val();
+
+        districtObject.html('<option value="">เลือกตำบล</option>');
+        
+        $.get('get_district.php?amphure_id=' + amphureId, function(data){
+            var result = JSON.parse(data);
+            $.each(result, function(index, item){
+                districtObject.append(
+                    $('<option></option>').val(item.id).html(item.name_th)
+                );
+            });
+        });
+    });
+});
