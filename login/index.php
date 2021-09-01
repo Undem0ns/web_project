04b9bcc46c5e026130bcf689dd0fd537e4285f99
yannapat112 <yannapat@kkumail.com>
2021-09-01 17:16:35 +0700

    login

--- login/index.php
+++ login/index.php
@@ -3,19 +3,22 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="css.css" rel="stylesheet">
     <title>Document</title>
 </head>
-<body  class="bg-secondary">
-<?php 
+
+<body>
+    <?php
     session_start();
     session_destroy();
-        
-  ?>
-    <div class="container " style="margin-top: 12%; ">
+
+    ?>
+    <!-- <div class="container " style="margin-top: 12%; ">
     <div class="row">
         <div class="col">
 
@@ -44,10 +47,32 @@
 
         </div>
     </div>
-</div>
+</div> -->
+
+
+    <div class="container">
+        <div class="row row-cols-2">
+            <div class="col">
+                
+            </div>
+            <div class="col-sm-4 ">
+                <div class="card-body " style="padding-top: 14rem;">
+                    <h5 class="card-title">เข้าสู่ระบบ</h5>
+                    <h6 class="card-subtitle mb-2 text-muted">sign in your account</h6>
+
+                    <form action="login_check.php" method="POST">
+                        <div class="mb-3">
+                            <input type="email" name="user_email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email">
+                        </div>
+                        <div class="mb-3">
+                            <input type="password" name="user_password" class="form-control" id="exampleInputPassword1" placeholder="password">
+                        </div>
+
+                        <button type="submit" name="submit" class="btn btn-primary">เข้าสู่ระบบ</button>
+                    </form>
+
+                </div>
+            </div>
 </body>
+
 </html>
-
-
-
-
